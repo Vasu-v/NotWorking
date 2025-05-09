90902321844e3550e2e079ba4aaabfa14fe16728
Vasu-v <vasanth8.allam@gmail.com>
2025-05-09 09:43:04 +0530

    add AuthService with user registration and retrieval functionality; include unit tests and initial user data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from './services/auth.service';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Depression';
+ message = " you are in that state";
+  userlist: Array<any> = [];
+ constructor(private auth: AuthService) { }
+  ngOnInit() {
+    this.getuserslist();
+  }
+ getuserslist(){
+     this.auth.getUsers().subscribe((data) => {
+
+      this.userlist = data
+      console.log(data);
+      // this.message = data;
+    }, (error) => { 
+      console.log(error);
+    }
+    );
+ }
 }
