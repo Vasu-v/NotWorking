4040a2817a9c90cac569a8ec8d255ca9402d30f8
Vasu-v <vasanth8.allam@gmail.com>
2025-05-10 18:02:41 +0530

    testing

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,16 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing'; // ✅ import this
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
-  let service: AuthService;
-
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], // ✅ add this
+      providers: [AuthService]
+    });
   });
 
   it('should be created', () => {
+    const service: AuthService = TestBed.inject(AuthService);
     expect(service).toBeTruthy();
   });
 });
