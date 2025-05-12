acd87390586e7cec83245eb1d9b5074a348ad051
Vasu-v <vasanth8.allam@gmail.com>
2025-05-12 13:33:58 +0530

    dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component , Pipe } from '@angular/core';
 import { AuthService } from './services/auth.service';
-import { HttpClient } from '@angular/common/http';
+// import { Router } from '@angular/router';
+import { Router } from '@angular/router';
+// import { HttpClient } from '@angular/common/http';
+// import { catchError, concatMap } from 'rxjs/operators';
+// import { Pipe } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,7 +14,7 @@ export class AppComponent {
   title = 'Depression';
  message = " you are in that state";
   userlist: any = [];
- constructor(private auth: AuthService) { }
+ constructor(private auth: AuthService, private router: Router) { }
   ngOnInit() {
     this.getuserslist();
   }
@@ -19,10 +23,30 @@ export class AppComponent {
 
       this.userlist = data
       console.log(data);
+      debugger
+      this.router.navigate(['dashboard'])
       // this.message = data;
     }, (error) => { 
       console.log(error);
     }
     );
  }
-}
+
+ getUserListByid()  {
+ 
+  // this.auth.getUserList().pipe(
+  //   concatMap((users : any) => {
+  //     console.log(users), "userList";
+  //     return this.auth.getUserById(users[0].id);
+  //   }),
+  //   catchError((error) => {
+  //     console.log("Error fetching user", error);
+  //     return [[]];
+  //   })
+
+  // ).subscribe((data : any) => { 
+  //   console.log(data);
+  // }
+  // );
+
+}}
---
+++ src/app/user-details/user-details.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-user-details',
+  templateUrl: './user-details.component.html',
+  styleUrls: ['./user-details.component.css']
+})
+export class UserDetailsComponent {
+
+  // constructor( private auth : AuthService) { }
+
+  // ngOnInit(): void {
+   
+
+  // Add any methods or properties needed for this component
+
+}
